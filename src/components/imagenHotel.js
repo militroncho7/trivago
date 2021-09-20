8073887eb281816e55cb1d44706f2075b88cab39
militroncho7 <vm.moralesruiz@gmail.com>
2021-09-20 09:49:18 +0200

    start project

--- src/components/imagenHotel.js
+++ src/components/imagenHotel.js
@@ -46,7 +46,7 @@ const ImagenHotel = () => {
       }
     `);
 
-    // console.log(image.sharp.fluid);
+    // console.log(image.sharp.fluid)
 
     return (
         <ImageBackground tag="section" fluid={image.sharp.fluid} fadeIn="soft">
